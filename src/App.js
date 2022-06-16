6f5887cdb69d8d5f7b75fd45810952faa5483ae0
Oussama <guezguezoussama@gmail.com>
2022-06-16 23:57:27 +0200

    search by rate added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import { useEffect,useState } from 'react';
+import ReactStars from 'react-rating-stars-component';
 import './App.css';
 import Movie from './components/Movie';
 
 
-const FEATURED_API ="https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1";
+const FEATURED_API ="https://api.themoviedb.org/3/discover/movie?api_key=04c35731a5ee918f014970082a0088b1&page=1";
 const SEARCH_API ="https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query="
 
 function App() {
   const [movies, setMovies] = useState([]);
   const [searchInput, setSearchInput] = useState('');
+  
   
 
   useEffect(()=>{
@@ -32,6 +34,11 @@ function App() {
     }
 };
 
+const getMoviesByRate = (rate) =>{
+  setMovies(movies.filter(el => el.vote_average>= rate));
+  console.log("aa",movies);
+}
+
   const handleOnChange = (e) => {
     setSearchInput(e.target.value);
   }
@@ -46,7 +53,13 @@ function App() {
             value={searchInput}
             onChange={handleOnChange}
             />
+            <ReactStars count={10}
+                            onChange={getMoviesByRate}
+                            size={20}
+                            isHalf={true}
+                            activeColor="#ffd700"/>
          </form>
+         
         </header>
       <div className='movie-container'>
         {movies.map(movie=> 
